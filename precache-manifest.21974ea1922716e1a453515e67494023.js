47b40d2d2673b29581f21d97977cfe380390bea4
Hemendra Khatik <hemendra.khatik010698@gmail.com>
2020-03-25 23:49:21 +0530

    Updates

---
+++ precache-manifest.21974ea1922716e1a453515e67494023.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ddb78911d44d0e304543a2ce99881d9",
+    "url": "/QuizApp/index.html"
+  },
+  {
+    "revision": "a6cbb89816ec7d6b408a",
+    "url": "/QuizApp/static/css/main.ce40bc82.chunk.css"
+  },
+  {
+    "revision": "1eba195d50c842915c1c",
+    "url": "/QuizApp/static/js/2.3ba009f5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/QuizApp/static/js/2.3ba009f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6cbb89816ec7d6b408a",
+    "url": "/QuizApp/static/js/main.df634540.chunk.js"
+  },
+  {
+    "revision": "ca341f5aa9f028892bc2",
+    "url": "/QuizApp/static/js/runtime-main.ccf39e61.js"
+  }
+]);
